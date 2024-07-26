9cc9839511a05dbc97d8994fbbec4b499fcb20b1
Daniel Montero <daniel.montero.mendez@kyndryl.com>
2024-07-25 20:02:28 -0600

    Feat: rewrite script to make it more robust

--- esp32/arduino-esp32/temp-node/temp-node.ino
+++ esp32/arduino-esp32/temp-node/temp-node.ino
@@ -5,24 +5,24 @@
 #include <DallasTemperature.h>
 
 
-#define SERIAL_DEBUG_BAUD   115200
+#define SERIAL_DEBUG_BAUD 115200
 
-#define WIFI_AP_NAME        "Nour"
-#define WIFI_PASSWORD       "tinyfootprint"
+#define WIFI_AP_NAME "Nour"
+#define WIFI_PASSWORD "tinyfootprint"
 
-#define TOKEN               "s2or2c7dx7v4vb369fpl"
-#define THINGSBOARD_SERVER  "34.122.121.12"
+#define TOKEN "s2or2c7dx7v4vb369fpl"
+#define THINGSBOARD_SERVER "34.122.121.12"
 
 // Data wire is plugged TO GPIO 4
-#define ONE_WIRE_BUS        4
+#define ONE_WIRE_BUS 4
 
 WiFiClient espClient;
-
-// the Wifi radio's status
-int status = WL_IDLE_STATUS;
+const char *hostname = "Temperature monitor";
+volatile int wifi_cnt = 0;
 
 Arduino_MQTT_Client mqttClient(espClient);
 ThingsBoard tb(mqttClient);
+volatile int mqtt_cnt = 0;
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -38,35 +38,54 @@ DeviceAddress tempDeviceAddress;
 
 float intTemp, extTemp;
 
-uint32_t interval = 60*1000;
+uint32_t interval = 60 * 1000;
+long strt = 0L;
+TaskHandle_t xHandle = NULL;
 
-// Set to true if application is subscribed for the messages
-bool subscribed = false;
+void send_temp(void *pvParameters);
 
 void setup() {
   // Initialize serial for debugging
   Serial.begin(SERIAL_DEBUG_BAUD);
+  delay(10);
 
   // Connect to WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname(hostname);
+  //WiFi.config(local_IP, gateway, subnet);
   WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-  initWiFi();
-  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    wifi_cnt++;
+    Serial.println("Connecting to WiFi...");
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to the WiFi network");
+    Serial.print("IP address: ");
+    Serial.print(WiFi.localIP());
+    Serial.printf(" after %d tries\n", wifi_cnt);
+  } else {
+    Serial.println(" Not connected to the WiFi network");
+    // Serial.println("Restart ESP32");
+    // esp_restart();
+  }
+
   // Initialize temperature sensors
   sensors.begin();
-  
+
   // Grab a count of devices on the wire
   numberOfDevices = sensors.getDeviceCount();
-  
+
   // Locate devices on the bus
-  Serial.print("Locating devices...");
+  Serial.println("Locating devices...");
   Serial.print("Found ");
   Serial.print(numberOfDevices, DEC);
-  Serial.println(" devices.");
+  Serial.println(" devices");
 
   // Loop through each device, print out address
-  for(int i=0; i<numberOfDevices; i++){
+  for (int i = 0; i < numberOfDevices; i++) {
     // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i)){
+    if (sensors.getAddress(tempDeviceAddress, i)) {
       Serial.print("Found device ");
       Serial.print(i, DEC);
       Serial.print(" with address: ");
@@ -78,102 +97,106 @@ void setup() {
       Serial.print(" but could not detect address. Check power and cabling");
     }
   }
+
+  // Setup thread
+  Serial.println("Create send_temp thread");
+  xTaskCreatePinnedToCore(
+    send_temp,
+    "send_temp",
+    2048,
+    NULL,
+    6,         // Priority
+    &xHandle,  // Task Handle
+    1);        // Run on Core 1 for best sampling?
+  configASSERT(xHandle);
 }
 
-void loop() {
-  static uint32_t nextTime;
+void loop() {}
 
-  // Reconnect to WiFi, if needed
-  if (WiFi.status() != WL_CONNECTED) {
-    reconnect();
-    return;
-  }
+void send_temp(void *pvParameters) {
+  Serial.println("Inside send_temp");
 
-  // Reconnect to ThingsBoard, if needed
-  if (!tb.connected()) {
-    subscribed = false;
+  for (;;) {
+    static uint32_t nextTime;
+    vTaskDelay(1);
+    strt = xTaskGetTickCount();
 
-    // Connect to the ThingsBoard
-    Serial.print("Connecting to: ");
-    Serial.print(THINGSBOARD_SERVER);
-    Serial.print(" with token ");
-    Serial.println(TOKEN);
-    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
-      Serial.println("Failed to connect");
-      return;
+    // Check if it is a time to send data
+    if (millis() - nextTime >= interval) {
+      Serial.println("Send temperature");
+      mqtt_cnt = 0;
+
+      while (!tb.connected() && mqtt_cnt < 5) {
+        Serial.print("Connecting to: ");
+        Serial.print(THINGSBOARD_SERVER);
+        Serial.print(" with token ");
+        Serial.println(TOKEN);
+        if (tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+          Serial.println("Read temperatures");
+          sensors.requestTemperatures();  // Send the command to get temperatures
+
+          // Get temperature from first sensor
+          if (sensors.getAddress(tempDeviceAddress, 0)) {
+            // Output the device ID
+            Serial.print("Temperature for device: ");
+            Serial.println(0, DEC);
+            // Print the data
+            intTemp = sensors.getTempC(tempDeviceAddress);
+            Serial.print("Temp C: ");
+            Serial.println(intTemp);
+          }
+
+          // Get temperature from second sensor
+          if (sensors.getAddress(tempDeviceAddress, 1)) {
+            // Output the device ID
+            Serial.print("Temperature for device: ");
+            Serial.println(1, DEC);
+            // Print the data
+            extTemp = sensors.getTempC(tempDeviceAddress);
+            Serial.print("Temp C: ");
+            Serial.println(extTemp);
+          }
+
+          Serial.println("Sending data...");
+
+          // Uploads new telemetry to ThingsBoard using MQTT.
+          // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api
+          // for more details
+          tb.sendTelemetryData("intTemp", intTemp);
+          tb.sendTelemetryData("extTemp", extTemp);
+          // Process messages
+          if (tb.loop()) {
+            Serial.println("Message sent successfully!");
+          }
+
+          Serial.print("Free heap: ");
+          Serial.println(ESP.getFreeHeap());
+        } else {
+          Serial.print("Failed to connect to MQTT with WiFi state: ");
+          Serial.println(WiFi.status());
+          delay(1000);
+          mqtt_cnt++;
+        }
+        if (mqtt_cnt >= 5) {
+          Serial.println(" Not connected to MQTT server");
+          Serial.println("Restart ESP32");
+          esp_restart();
+        }
+      }
+
+      Serial.println("Disconnect form MQTT server");
+      tb.disconnect();
+
+      Serial.println("Increase next time by interval");
+      nextTime += interval;
     }
-  }
-  
-  // Check if it is a time to send temperature
-  if (millis() - nextTime >= interval) {
-    sensors.requestTemperatures(); // Send the command to get temperatures
-
-    // Get temperature from first sensor
-    if(sensors.getAddress(tempDeviceAddress, 0)) {
-      // Output the device ID
-      Serial.print("Temperature for device: ");
-      Serial.println(0, DEC);
-      // Print the data
-      intTemp = sensors.getTempC(tempDeviceAddress);
-      Serial.print("Temp C: ");
-      Serial.println(intTemp);
-    }
-
-    // Get temperature from second sensor
-    if(sensors.getAddress(tempDeviceAddress, 1)) {
-      // Output the device ID
-      Serial.print("Temperature for device: ");
-      Serial.println(1, DEC);
-      // Print the data
-      extTemp = sensors.getTempC(tempDeviceAddress);
-      Serial.print("Temp C: ");
-      Serial.println(extTemp);
-    }
-
-    Serial.println("Sending data...");
-
-    // Uploads new telemetry to ThingsBoard using MQTT. 
-    // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api 
-    // for more details
-    tb.sendTelemetryData("intTemp", intTemp);
-    tb.sendTelemetryData("extTemp", extTemp);
-
-    nextTime += interval;
-  }
-
-  // Process messages
-  tb.loop();
-}
-
-void initWiFi() {
-  Serial.println("Connecting to AP ...");
-  // attempt to connect to WiFi network
-
-  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected to AP");
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  status = WiFi.status();
-  if ( status != WL_CONNECTED) {
-    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("Connected to AP");
   }
 }
 
 // Function to print a device address
 void printAddress(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++){
+  for (uint8_t i = 0; i < 8; i++) {
     if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
+    Serial.print(deviceAddress[i], HEX);
   }
 }
